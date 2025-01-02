a531766bc1021d6fac293cf2d6679aee86ef3b3c
Denis Ogun <dogun@anduril.com>
2025-01-02 12:48:49 -0800

    Problem: When running the sample application locally, it would run into a path and CORS error
    Solution:
    Use Vite Proxy to proxy from the local URL to the upstream server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, ProxyOptions } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const proxy : Record<string, string | ProxyOptions> = {};
+  proxy[`/${env.VITE_BASE_URL}`] = {
+    target: `https://${env.VITE_BASE_URL}`,
+    changeOrigin: true,
+    rewrite: (path) => path.replace(`${env.VITE_BASE_URL}`, ''),
+  }
+
+  return {
+    server: {
+      proxy
+    },
+    plugins: [react()]
+  }
 })
